6a0c04eba7cf7fddf5e692806be8bf4d1fe5a726
Sapkota <dsapkota@hq.corp.viasat.com>
2017-05-30 16:20:10 -0600

    week-4-game

---
+++ assests/javascript/game.js
@@ -0,0 +1,96 @@
+$( document ).ready(function(){
+    var yourNumber = 0;
+    var wins = 0;
+    var losses = 0;
+    var yourTotalScore = 0;
+
+    var randomNumber = Math.floor(Math.random() * 12 +1);
+    $('#randomNumber').html(randomNumber);
+
+    var restart = function () {
+        //restart the variables that  are printing to the screen.
+        wins = 0;
+        losses = 0;
+        yourTotalScore = 0;
+        //then print them to the screen.
+        $("#yourTotalScore").html(yourTotalScore);
+        $('#wins').html(wins);
+        $("#losses").html(losses);
+        //generate another random number and print to screen.
+        var randomNumber = Math.floor(Math.random() * 12 +1);
+        $('#randomNumber').html(randomNumber);
+    }
+
+    // //grab the reset button with jQuery and then run the function restart on click.
+    // $('#reset-button').on('click', restart);
+
+     var newYourNumber =  function yourNumber(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+     };
+
+    $("#one").on("click", function(){
+        var randomOnClick = newYourNumber(1 , 12);
+        console.log(randomOnClick);
+        yourTotalScore = yourTotalScore + randomOnClick;
+        $("#yourTotalScore").html(yourTotalScore);
+
+        if (yourTotalScore == randomNumber) {
+            wins++;
+            $('#wins').html(wins);
+        }
+        if (yourTotalScore > randomNumber){
+            losses++;
+            $("#losses").html(losses);
+        }
+
+    });
+
+    $("#two").on("click", function(){
+       var randomOnClick = newYourNumber(1 , 12);
+        console.log(randomOnClick);
+        yourTotalScore = yourTotalScore + randomOnClick;
+        $("#yourTotalScore").html(yourTotalScore);
+
+        if (yourTotalScore == randomNumber) {
+            wins++;
+            $('#wins').html(wins);
+        }
+        if (yourTotalScore > randomNumber){
+            losses++;
+            $("#losses").html(losses);
+        }  
+    });
+
+    $("#three").on("click", function(){
+      var randomOnClick = newYourNumber(1 , 12);
+        console.log(randomOnClick);
+        yourTotalScore = yourTotalScore + randomOnClick;
+        $("#yourTotalScore").html(yourTotalScore);
+
+        if (yourTotalScore == randomNumber) {
+            wins++;
+            $('#wins').html(wins);
+        }
+        if (yourTotalScore > randomNumber){
+            losses++;
+            $("#losses").html(losses);
+        }  
+    });
+
+    $("#four").on("click", function(){
+      var randomOnClick = newYourNumber(1 , 12);
+        console.log(randomOnClick);
+        yourTotalScore = yourTotalScore + randomOnClick;
+        $("#yourTotalScore").html(yourTotalScore);
+
+        if (yourTotalScore == randomNumber) {
+            wins++;
+            $('#wins').html(wins);
+        }
+        if (yourTotalScore > randomNumber){
+            losses++;
+            $("#losses").html(losses);
+        }
+    });
+
+});
